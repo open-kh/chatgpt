84326e388929e623fe1bb1848eed2e19f2294008
iphearum <phearumgic@gmail.com>
2023-05-21 02:16:33 +0700

    merge

--- pm2-deploy.sh
+++ pm2-deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-COUNT=3
+COUNT=15
 for i in $(seq 1 $COUNT)
 do
-  PORT=300$i pm2 start yarn --name vm-bot -- start #--max-memory-restart 150M
+  PORT=300$i pm2 start yarn --name vm-bot -- start --max-memory-restart 150M
 done
 pm2 save
