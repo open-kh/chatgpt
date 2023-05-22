ad5cc5f45b866b46023ff2f9dca22217ab7add45
iphearum <phearumgic@gmail.com>
2023-05-22 19:54:36 +0700

    upgrade btn

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -260,22 +260,24 @@ export const ChatInput = ({
     <div className="absolute bottom-0 left-0 w-full border-transparent bg-gradient-to-b from-transparent via-white to-white pt-6 dark:border-white/20 dark:via-[#343541] dark:to-[#343541] md:pt-2">
       <div className="relative stretch mx-2 mt-4 flex flex-row gap-3 last:mb-2 md:mx-4 md:mt-[52px] md:last:mb-6 lg:mx-auto lg:max-w-3xl">
         <div className='absolute bottom-1.5 right-[1.35rem] z-30 flex flex-col max-sm:-bottom-2.5 max-sm:right-2.5'>
-          <div className={'transition-all ease-in-out duration-900'+(content?' sm:-mb-[42.5px] md:mb-0 md:mr-[40px]':'')}>
+          <div className={'transition-all ease-in-out duration-900'+(content?' max-sm:-mb-[42.5px] md:mb-0 md:mr-[40px]':'')}>
             {messageIsStreaming && (
               <button
-                className="mx-auto mb-3 flex w-fit items-center gap-3 rounded-3xl border border-neutral-200 bg-white py-1.5 px-4 text-black hover:opacity-50 dark:border-neutral-600 dark:bg-[#343541] dark:text-white md:mb-0 md:mt-2"
+                className="mx-auto mb-3 flex w-fit items-center gap-3 rounded-3xl border border-neutral-200 bg-white py-1.5 px-4 text-black hover:opacity-50 dark:border-neutral-600 dark:bg-[#343541] dark:text-white md:mb-0 md:mt-2 max-sm:py-2 max-sm:px-2"
                 onClick={handleStopConversation}
               >
-                <IconPlayerStop size={16} /> {t('Stop Generating')}
+                <IconPlayerStop size={16} />
+                <span className='max-sm:hidden'>{t('Stop Generating')}</span>
               </button>
             )}
             {!messageIsStreaming &&
               selectedConversation &&
               selectedConversation.messages.length > 0 && (
                 <button
-                  className="mx-auto mb-3 flex w-fit items-center gap-3 rounded-3xl border border-neutral-200 bg-white py-1.5 px-4 text-black hover:opacity-50 dark:border-neutral-600 dark:bg-[#343541] dark:text-white md:mb-0 md:mt-2"
+                  className="mx-auto mb-3 flex w-fit items-center gap-3 rounded-3xl border border-neutral-200 bg-white py-1.5 px-4 text-black hover:opacity-50 dark:border-neutral-600 dark:bg-[#343541] dark:text-white md:mb-0 md:mt-2 max-sm:py-2 max-sm:px-2"
                   onClick={onRegenerate}
-                ><IconRepeat size={16} /> {t('Regenerate')}
+                ><IconRepeat size={16} />
+                <span className='max-sm:hidden'>{t('Regenerate')}</span>
                 </button>
               )}
           </div>
--- next.config.js
+++ next.config.js
@@ -2,15 +2,10 @@ const { i18n } = require('./next-i18next.config');
 const withPWA = require('next-pwa')
 
 /** @type {import('next').NextConfig} */
-const nextConfig = withPWA({
+// const nextConfig = withPWA();
+const nextConfig = {
   i18n,
   reactStrictMode: true,
-  pwa: {
-    dest: 'public',
-    mode: 'standalone',
-    // swSrc: '',
-    disable: process.env.NODE_ENV === 'development'
-  },
   webpack(config, { isServer, dev }) {
     config.experiments = {
       asyncWebAssembly: true,
@@ -18,6 +13,6 @@ const nextConfig = withPWA({
     };
     return config;
   }
-});
+};
 
 module.exports = nextConfig;
