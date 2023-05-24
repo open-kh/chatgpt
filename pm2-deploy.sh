a0142e74400e12d7de8b72ddc6f1ae5b844b2a88
iphearum <phearumgic@gmail.com>
2023-05-24 11:36:05 +0700

    change restart app

--- pm2-deploy.sh
+++ pm2-deploy.sh
@@ -5,7 +5,7 @@ pm2 kill
 COUNT=15
 for i in $(seq 1 $COUNT)
 do
-  PORT=300$i pm2 --max-memory-restart 150M --name vm-bot start yarn -- start
+  PORT=300$i pm2 --max-memory-restart 85M --name vm-bot start yarn -- start
 done
 
 pm2 save
