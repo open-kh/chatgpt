6f9a48162bf53ae1cb970306c5bc6f77f8f434ca
open-kh <phearum.nop.kh@gmail.com>
2023-05-25 17:30:15 +0000

    update pm2

--- pm2-deploy.sh
+++ pm2-deploy.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
+# pm2 flush
 pm2 kill
+#pm2 delete all
 
-COUNT=15
+COUNT=5
 for i in $(seq 1 $COUNT)
 do
-  PORT=300$i pm2 --max-memory-restart 85M --name vm-bot start yarn -- start
+  PORT=300$i pm2 --wait-ready --listen-timeout 1000 --cron-restart="*/5 * * * *" --name vm-bot-$i start yarn -- start
+  # pm2 scale vm-bot-$i +3
 done
+# PORT=3001 pm2 --wait-ready --listen-timeout 2000 --cron-restart="*/25 * * * *" --name vm-bot start yarn -- start
+# pm2 scale vm-bot +5
 
 pm2 save
