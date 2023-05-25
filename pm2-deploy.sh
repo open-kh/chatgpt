7ad6e0a752ed80d375a9e9876145801eb8c2d327
iphearum <phearumgic@gmail.com>
2023-05-26 00:52:43 +0700

    change

--- pm2-deploy.sh
+++ pm2-deploy.sh
@@ -7,7 +7,7 @@ pm2 kill
 COUNT=5
 for i in $(seq 1 $COUNT)
 do
-  PORT=300$i pm2 --wait-ready --listen-timeout 1000 --cron-restart="*/$i * * * *" --name vm-bot-$i start yarn -- start
+  PORT=300$i pm2 --wait-ready --listen-timeout 1000 --cron-restart="*/1$i * * * *" --name vm-bot-$i start yarn -- start
   # pm2 scale vm-bot-$i +3
 done
 # PORT=3001 pm2 --wait-ready --listen-timeout 2000 --cron-restart="*/25 * * * *" --name vm-bot start yarn -- start
