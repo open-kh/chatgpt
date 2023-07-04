d9999a7411ecc106cd0c47b462f23ad1dab01595
iphearum <phearumgic@gmail.com>
2023-07-04 22:29:36 +0700

    change style

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -489,7 +489,7 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
                     ))}
 
                     {loading && <ChatLoader />}
-                    <ChatLoader />
+                    {/* <ChatLoader /> */}
 
                     <div
                       className="h-[162px] bg-white dark:bg-[#343541]"
