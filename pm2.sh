19151643f226e76ad3cdd338b79bcdf0e2d96f5c
iphearum <phearumgic@gmail.com>
2023-07-04 23:59:54 +0700

    add pm2

---
+++ pm2.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# pm2 flush
+pm2 kill
+#pm2 delete all
+
+COUNT=5
+for i in $(seq 1 $COUNT)
+do
+  PORT=300$i pm2 --name ai-chatbot-$i start yarn -- start
+  # pm2 scale vm-bot-$i +3
+done
+# PORT=3001 pm2 --wait-ready --listen-timeout 2000 --cron-restart="*/25 * * * *" --name vm-bot start yarn -- start
+# pm2 scale vm-bot +5
+
+pm2 save
